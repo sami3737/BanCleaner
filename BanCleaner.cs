5b178e6d12b542775c9ead69ca4be2bec3422f9e
sami3737 <46924423+sami3737@users.noreply.github.com>
2024-10-31 17:29:12 +0100

    Add files via upload

---
+++ BanCleaner.cs
@@ -0,0 +1,21 @@
+﻿namespace Oxide.Plugins
+{
+    [Info("BanCleaner", "sami37", "1.0.0")]
+    public class BanCleaner : RustPlugin
+    {
+        void OnUserBanned(string name, string id, string address, string reason)
+        {
+            BasePlayer player = BasePlayer.Find(id);
+            if (player != null)
+                DestroyEntity(player);
+        }
+
+        private void DestroyEntity(BasePlayer player)
+        {
+            //if (player.net.connection.authLevel == 0)
+            foreach (var entity in UnityEngine.Object.FindObjectsOfType<BaseEntity>())
+                if (entity.OwnerID == player.userID)
+                    entity.Kill();
+        }
+    }
+}
